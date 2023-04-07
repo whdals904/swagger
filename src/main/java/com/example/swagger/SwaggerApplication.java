6f4fe29259f957ace41c96c56381f8be0226b9c2
김종민 <whdals904@naver.com>
2023-04-07 15:01:52 +0900

    static method 테스트

--- src/main/java/com/example/swagger/SwaggerApplication.java
+++ src/main/java/com/example/swagger/SwaggerApplication.java
@@ -1,5 +1,7 @@
 package com.example.swagger;
 
+import com.example.swagger.common.HttpComponent;
+import com.example.swagger.common.HttpMode;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -11,12 +13,14 @@ import java.io.IOException;
 public class SwaggerApplication {
 
 
-
 	public static void main(String[] args) throws IOException {
 
 		SpringApplication.run(SwaggerApplication.class, args);
 
-
+		HttpComponent hc = HttpComponent.getHttpComponent(HttpMode.TEST);
+		System.out.println("hc TEST = " + hc);
+		HttpComponent hc1 = HttpComponent.getHttpComponent(HttpMode.PRODUCTON);
+		System.out.println("hc PRODUCTON = " + hc1);
 
 	}
 }
---
+++ src/main/java/com/example/swagger/common/HttpMode.java
@@ -0,0 +1,5 @@
+package com.example.swagger.common;
+
+public enum HttpMode {
+    TEST, PRODUCTON
+}
